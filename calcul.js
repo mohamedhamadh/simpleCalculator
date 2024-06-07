100016427ebf58c254094f9e2155633e0c13b63b
mohamedhamadh <140078237+mohamedhamadh@users.noreply.github.com>
2024-06-07 18:55:22 +0300

    Add files via upload

---
+++ calcul.js
@@ -0,0 +1,19 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+
+    display.value += input;
+
+}
+
+function clearDisplay(){
+    display.value ="";
+}
+function caclculate(){
+  try{
+    display.value=eval(display.value);
+  }
+  catch(error){
+    display.value="Error";
+  }
+}
